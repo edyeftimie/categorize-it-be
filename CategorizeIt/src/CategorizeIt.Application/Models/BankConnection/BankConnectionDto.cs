a046712e596785d624772aa1ac9858c948bd956e
edyeftimie <eftimie.eduard28@gmail.com>
2026-07-01 11:42:49 +0300

    little modifies

--- CategorizeIt/src/CategorizeIt.Application/Models/BankConnection/BankConnectionDto.cs
+++ CategorizeIt/src/CategorizeIt.Application/Models/BankConnection/BankConnectionDto.cs
@@ -1,16 +1,5 @@
 namespace CategorizeIt.Application.Models.BankConnections;
 
-public class BankAccountDto
-{
-    public Guid Id { get; set; }
-    public string Uid { get; set; } = string.Empty;
-    public string? Iban { get; set; }
-    public string? Name { get; set; }
-    public string Currency { get; set; } = string.Empty;
-    public string? CashAccountType { get; set; }
-    public DateTime? LastSyncedAt { get; set; }
-}
-
 public class BankConnectionDto
 {
     public Guid Id { get; set; }
